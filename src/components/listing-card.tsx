5d3ae810d5eb214e4a22812ebae8644be149995b
Abraham <abrahamugbeshe@gmail.com>
2024-10-30 00:20:51 +0100

    wip: consuming search api, added loading state

--- src/components/listing-card.tsx
+++ src/components/listing-card.tsx
@@ -1,11 +1,12 @@
 import { VerifiedIcon } from "lucide-react";
 import Image from "next/image";
 import React from "react";
+import { Skeleton } from "./ui/skeleton";
 
 const img =
   "https://images.unsplash.com/photo-1499955085172-a104c9463ece?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
-const ListingCard = () => {
+const ListingCard = ({ property }: { property: PropertyType }) => {
   return (
     <div className="w-full">
       <div className="w-full aspect-[372/302]  rounded-lg overflow-hidden relative border">
@@ -21,16 +22,13 @@ const ListingCard = () => {
           className="flex items-center justify-between
         "
         >
-          <span className="flex items-center gap-2 text-primary text-sm font-medium">
+          <span className="flex items-center gap-1 text-primary text-sm font-medium">
             <VerifiedIcon className=" fill-primary stroke-white" size={20} />
             Verified
           </span>
           <span className="text-xs text-gray-500 font-medium">5 days ago</span>
         </div>
-        <p className="text-base font-bold line-clamp-1">
-          Elegant 5-room apartment with spacious rooftop terrace and stunning
-          views
-        </p>
+        <p className="text-base font-bold line-clamp-1">{property.title}</p>
         <p className="text-xs text-gray-500 font-medium">
           Reichsratsstraße 13, 1010 Vienna
         </p>
@@ -47,3 +45,20 @@ const ListingCard = () => {
 };
 
 export default ListingCard;
+
+export const ListingCardSkeleton = () => (
+  <div className="w-full">
+    <Skeleton className="w-full aspect-[372/302]  rounded-lg overflow-hidden relative border" />
+
+    <div className="flex flex-col gap-y-3 p-2">
+      <div className="flex items-center justify-between">
+        <Skeleton className="w-20 h-4" />
+        <Skeleton className="w-20 h-4" />
+      </div>
+      <Skeleton className="w-full h-6" />
+      <Skeleton className="w-full h-4" />
+      <Skeleton className="w-1/2 h-6" />
+      <Skeleton className="w-1/2 h-4" />
+    </div>
+  </div>
+);
---
+++ types.d.ts
@@ -0,0 +1,71 @@
+interface OwnerType {
+  country: string;
+  name: string;
+  email: string;
+  phone: string;
+  description: string | null;
+  vat: string | null;
+  billingAddress: string | null;
+}
+
+interface UserType {
+  id: number;
+}
+
+interface MediaType {
+  type: string;
+  name: string;
+  cdnUrl: string;
+  sort: number;
+  title: string;
+  bluredDataURL: string;
+  id: number;
+}
+
+interface PropertyType {
+  owner: OwnerType;
+  user: UserType;
+  title: string;
+  abstract: string;
+  address: string;
+  addressDoor: string;
+  zip: string;
+  city: string;
+  country: string;
+  rooms: number;
+  roomsBed: number;
+  roomsBath: number;
+  size: number;
+  rent: number;
+  rentUtilities: number;
+  rentFull: number | null;
+  rentDeposit: number | null;
+  amenities: number[];
+  amenitiesTexts: Record<number, string>;
+  location: [number, number];
+  locationReal: [number, number] | null;
+  createdAt: string;
+  updatedAt: string;
+  type: number;
+  subType: number;
+  condition: number;
+  accessibility: number;
+  unitType: string;
+  rentType: string;
+  floorType: number;
+  heatingType: number;
+  leaseDuration: number;
+  availableFrom: string | null;
+  highlight: string | null;
+  active: boolean;
+  verified: boolean;
+  deleted: boolean;
+  autoRenewUntil: string | null;
+  lastRenewAt: string | null;
+  constructionYear: number | null;
+  modernisationYear: number | null;
+  floor: number | null;
+  id: number;
+  media: MediaType[];
+  tenements: unknown[];
+}
