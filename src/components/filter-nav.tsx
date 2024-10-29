a93a34b6944b90251bcf817c11887e0f69a973de
Abraham <abrahamugbeshe@gmail.com>
2024-10-29 22:22:36 +0100

    added all filter design

--- src/components/filter-nav.tsx
+++ src/components/filter-nav.tsx
@@ -1,16 +1,26 @@
-import { ChevronDownIcon } from "lucide-react";
+import { ChevronDownIcon, SlidersHorizontalIcon } from "lucide-react";
 import React from "react";
 
 const FilterNav = () => {
   return (
     <nav className="">
-      <ul className="container mx-auto  py-4 flex items-center space-x-4">
+      <ul className="container mx-auto  py-5 flex items-center space-x-4">
         {filter_methods.map((method) => (
-          <li key={method} className="text-sm gap-1 flex items-center">
+          <li key={method} className="text-sm gap-2 flex items-center">
             <span>{method}</span>
-            <ChevronDownIcon size={16} />
+            <ChevronDownIcon size={14} />
           </li>
         ))}
+        <li className="text-sm gap-2 flex items-center">
+          <span>
+            Price: <span className="text-gray-500">€300 - €500</span>
+          </span>
+          <ChevronDownIcon size={14} />
+        </li>
+        <li className="text-sm gap-2 flex items-center">
+          <span>All</span>
+          <SlidersHorizontalIcon size={14} />
+        </li>
       </ul>
     </nav>
   );
